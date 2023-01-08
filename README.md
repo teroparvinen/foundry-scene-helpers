f96a1e8fc860d4f5f2bc3fa30bae32f1d93fd823
Tero Parvinen <tero.parvinen@gmail.com>
2023-01-08 10:34:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Scene Helpers for the Foundry VTT
+
+A couple of user interface modifications to help handling scenes a bit easier:
+
+- Clicking on a scene in the scene list views the scene instead of opening its configuration
+- When the currently viewed scene has "Toggle Navigation" enabled, a pin icon will be shown in the scene entry to indicate it will stay open when navigating away from it
+- Clicking on a scene in the navigation bar with Alt/Command pressed will toggle the navigation/pin status of that scene
+- Double clicking the active scene will open the scene notes or the scene configuration (selectable in settings)
---
+++ module/scene-helpers.js
@@ -0,0 +1,113 @@
+
+Hooks.on("init", () => {
+    game.settings.register("scene-helpers", "doubleclick", {
+        name: "scene-helpers.setting-doubleclick",
+        scope: "world",
+        config: true,
+        type: String,
+        choices: {
+            notes: "scene-helpers.setting-doubleclick-notes",
+            config: "scene-helpers.setting-doubleclick-config"
+        },
+        default: "notes"
+    });
+});
+
+Hooks.on("setup", () => {
+    libWrapper.register("scene-helpers", "SidebarDirectory.prototype._onClickDocumentName", onClickDocumentName, "MIXED");
+    libWrapper.register("scene-helpers", "SceneNavigation.prototype._onClickScene", onClickScene, "MIXED");
+});
+
+function onClickDocumentName(wrapped, event) {
+    const element = event.currentTarget;
+    const documentId = element.parentElement.dataset.documentId;
+    const document = this.constructor.collection.get(documentId);
+
+    if (document.documentName == "Scene" && document.testUserPermission(game.user, "LIMITED")) {
+        if ((event.ctrlKey || event.metaKey) && !event.altKey) {
+            document.activate();
+        } else if (!(event.ctrlKey || event.metaKey) && event.altKey) {
+            if (game.user.isGM) {
+                const sheet = document.sheet;
+                if (sheet.rendered) {
+                    sheet.maximize();
+                    sheet.bringToTop();
+                } else {
+                    sheet.render(true);
+                }
+            }
+        } else {
+            document.view();
+        }
+
+        return true;
+    } else {
+        return wrapped(event);
+    }
+}
+
+function togglePin(event) {
+    if (game.user.isGM) {
+        const sceneId = event.target.closest('.scene').dataset.sceneId;
+        const scene = game.scenes.get(sceneId);
+        scene.update({ navigation: !scene.navigation });
+    }
+}
+
+function onClickScene(wrapped, event) {
+    if (event.altKey || event.metaKey) {
+        togglePin(event);
+    } else {
+        wrapped(event);
+    }
+}
+
+Hooks.on("renderSceneNavigation", (app, html, data) => {
+    if (game.user.isGM) {
+        const viewed = html.find('.scene.view');
+        if (viewed.length) {
+            const viewedSceneId = viewed[0].dataset.sceneId;
+            const scene = game.scenes.get(viewedSceneId);
+            const isPinned = scene.navigation;
+            const isActive = scene.active;
+            if (isPinned) {
+                if (isActive) {
+                    html.find('.scene.view .scene-name i').after('<i class="fas fa-map-pin pin-icon"></i>');
+                } else {
+                    html.find('.scene.view .scene-name').prepend('<i class="fas fa-map-pin pin-icon"></i>');
+                }
+            }
+        }
+
+        viewed
+            .off("click")
+            .on("click", function(event) {
+                if (event.altKey || event.metaKey) {
+                    togglePin(event);
+                }
+            })
+            .on("dblclick", function(event) {
+                const viewedSceneId = this.dataset.sceneId;
+                const scene = game.scenes.get(viewedSceneId);
+                const op = game.settings.get("scene-helpers", "doubleclick");
+    
+                if (op === "notes") {
+                    const entry = scene.journal;
+                    if (entry) {
+                        const sheet = entry.sheet;
+                        const options = {};
+                        if (scene.journalEntryPage) options.pageId = scene.journalEntryPage;
+                        sheet.render(true, options);
+                    }
+                } else if (op === "config") {
+                    const sheet = scene.sheet;
+                    if (sheet.rendered) {
+                        sheet.maximize();
+                        sheet.bringToTop();
+                    } else {
+                        sheet.render(true);
+                    }
+                }
+            });
+    }
+});
